8b5bc1f69fe328c5ee4e62d7076be8a919b0d704
TheSilentIce <srikaryadlapati@gmail.com>
2024-04-01 22:25:06 -0600

    Can add pins, will finish logic behind these tomorrow

--- src/components/Map-Utilities/map.tsx
+++ src/components/Map-Utilities/map.tsx
@@ -1,32 +1,59 @@
-"use client";
-
 import {
-    APIProvider,
-    AdvancedMarker,
-    InfoWindow,
-    Map,
-    Pin,
-    useMap
+  APIProvider,
+  AdvancedMarker,
+  Map,
 } from "@vis.gl/react-google-maps"
 
-import React from 'react';
+import React, { useState } from 'react';
+
+interface MarkerPosition {
+  lat: number;
+  lng: number;
+}
+
+const mapOptions = {
+  key: "API_KEY",
+  position: { lat: 43.6532, lng: -79.3832 },
+  mapId: "MAP_ID",
+}
 
 export default function Pages() {
-    const mapOptions = {
-        key : "API_STRING",
-        position : {lat : 43.6532, lng:-79.3832},
-        mapId : "MAP_ID",
-    }
+  const [markers, setMarkers] = useState<MarkerPosition[]>([]);
 
-    return (
-        <div style={{ height: "100vh", width: "100%" }}>
-          <APIProvider apiKey={mapOptions.key}>
-            <Map
-            defaultZoom={9}
-            defaultCenter={mapOptions.position}
-            mapId={mapOptions.mapId}
-            />
-          </APIProvider>
-        </div>
-      );
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <APIProvider apiKey={mapOptions.key}>
+        <MapComponent markers={markers} setMarkers={setMarkers} />
+      </APIProvider>
+    </div>
+  );
+}
+
+function MapComponent({ markers, setMarkers }: { markers: MarkerPosition[], setMarkers: React.Dispatch<React.SetStateAction<MarkerPosition[]>> }) {
+  const getBounds = (event) => {
+    const lat = event.detail.latLng.lat;
+    const lng = event.detail.latLng.lng;
+    console.log(lat, lng);
+    const newMarker = {
+      lat: lat,
+      lng: lng
+    };
+    const updatedMarkers = [...markers, newMarker];
+    setMarkers(updatedMarkers);
+  };
+
+  return (
+    <Map
+      defaultZoom={5}
+      defaultCenter={mapOptions.position}
+      mapId={mapOptions.mapId}
+      onClick={getBounds}
+    >
+      <AdvancedMarker position={{ lat: 50, lng: 50 }}></AdvancedMarker>
+
+      {markers.map((marker, idx) => (
+        <AdvancedMarker key={idx} position={marker}></AdvancedMarker>
+      ))}
+    </Map>
+  );
 }
